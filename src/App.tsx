5e34dfab94c7ee09e6963b6cec4790dadee46121
brunnoanjosfoursys <82662405+brunnoanjosfoursys@users.noreply.github.com>
2022-06-21 10:42:25 -0300

    finalizacao da aula 01

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { gql, useQuery } from '@apollo/client'
+import { useEffect } from 'react'
+import { client } from './lib/apollo'
+
+const GET_LESSONS_QUERY = gql`
+  query { 
+    lessons {
+      id
+      title
+    }
+  }
+`
+
+interface Lesson {
+  id: string;
+  title: string;
+}
+
+export function App() {
+  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY);
+  
+
+  return (
+    <ul className="">
+      {data?.lessons.map(lesson => {
+        return <li key={lesson.id}>{lesson.title}</li>
+      })}
+    </ul>
+  )
+}
